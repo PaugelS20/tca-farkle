9a0d4f57c1802a44b2c724f1ad8152222f98b397
Sam Paugel <shpaugel@madisoncollege.edu>
2023-05-02 13:47:04 -0500

    updated name id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.paugel.farkle',
   appName: 'tca-farkle',
   webDir: 'build',
   bundledWebRuntime: false
