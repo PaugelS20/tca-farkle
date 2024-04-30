9ccaea5317d9137a152e5089aca7e22a24028cc9
Sam Paugel <shpaugel@madisoncollege.edu>
2024-04-30 15:50:48 -0500

    New Build for github pages

---
+++ docs/static/js/841.7f566244.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.7f566244.chunk.js.LICENSE.txt */
+"use strict";(globalThis.webpackChunktca_farkle=globalThis.webpackChunktca_farkle||[]).push([[841],{841:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>o});var r=n(196),s=n(743),a=n(811);const o=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,o=document.elementFromPoint(t/2,n/2);if(!o)return;const i=(0,s.a)(o);i&&new Promise((e=>(0,a.c)(i,e))).then((()=>{(0,r.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,s.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.7f566244.chunk.js.map
